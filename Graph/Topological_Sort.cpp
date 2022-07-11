6ee80a1ac6b9ed78e0917a68ab3af6186b743b53
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-07-11 14:50:36 +0530

    Update Topological_Sort.cpp

--- Graph/Topological_Sort.cpp
+++ Graph/Topological_Sort.cpp
@@ -1,50 +1,36 @@
-#include <bits/stdc++.h>
-#include <queue>
-void solve(vector<vector<int>>&adj,vector<int>&indeg,vector<int>&ans,queue<int>&q){
-   
-   while(!q.empty()){
-       int t=q.front();
-       q.pop();
-       ans.push_back(t);
-       
-       for(auto it:adj[t]){
-           
-           indeg[it]--;
-           
-           if(indeg[it]==0){
-               q.push(it);
-           }
-       }
-   }
-       return ;
-   }
-
-vector<int> topologicalSort(vector<vector<int>> &edges, int V, int e)  {
-   vector<vector<int>>adj(V+1,vector<int>());
-    
-    for(int i=0;i<e;i++){
-        adj[edges[i][0]].push_back(edges[i][1]);
+#include<bits/stdc++.h>
+void dfs(vector<int> gp[],vector<bool>&vis,stack<int> &st,int i)
+{
+    vis[i] = true;
+    for(auto it:gp[i])
+    {
+        if(!vis[it])
+            dfs(gp,vis,st,it);
     }
-    
-   vector<int>indeg(V+1,0);
-      queue<int>q;
-      vector<int>ans;
-      
-      for(int i=0;i<V;i++){
-          for(int j=0;j<adj[i].size();j++){
-            indeg[adj[i][j]]++;
-          }
-      }
-      for(int i=0;i<V;i++){
-          if(indeg[i]==0){
-              q.push(i);
-          }
-      }
-     
-          if(!q.empty()) solve(adj,indeg,ans,q);
-          
-      
-      return ans;
+    st.push(i);
     
 }
-
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+    vector<int> gp[v];
+    for(int i=0;i<e;i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        gp[u].push_back(v);
+    }
+    stack<int>st;
+    vector<int>ans;
+    vector<bool> vis(v,false);
+    for(int i=0;i<v;i++)
+    {
+        if(!vis[i])
+           dfs(gp,vis,st,i);
+    }
+    while(!st.empty())
+    {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
